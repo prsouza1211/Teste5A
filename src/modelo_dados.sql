eddd709d8f84c5ed872c5485d7f39a609f62e601
Paulo Souza <paulobeto1211@gmail.com>
2017-07-16 23:04:03 -0300

    Corrigindo o script de criação do BD.

--- src/modelo_dados.sql
+++ src/modelo_dados.sql
@@ -1,21 +1,45 @@
-CREATE DATABASE teste;
+CREATE DATABASE IF NOT EXISTS `teste` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `teste`;
 
-CREATE TABLE tb_atividade (
-  id_atividade INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_status INTEGER UNSIGNED NOT NULL,
-  nm_atividade VARCHAR(255) NOT NULL,
-  ds_atividade TEXT NOT NULL,
-  dt_inicio DATE NOT NULL,
-  dt_fim DATE NULL,
-  situacao BOOL NULL DEFAULT TRUE,
-  PRIMARY KEY(id_atividade),
-  INDEX tb_atividade_FKIndex1(id_status)
-);
+CREATE TABLE `tb_status` (
+  `id_status` int(10) UNSIGNED NOT NULL,
+  `nm_status` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE tb_status (
-  id_status INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nm_status VARCHAR(100) NULL,
-  PRIMARY KEY(id_status)
-);
+--
+-- Extraindo dados da tabela `tb_status`
+--
 
+INSERT INTO `tb_status` (`id_status`, `nm_status`) VALUES
+(1, 'Pendente'),
+(2, 'Em Desenvolvimento'),
+(3, 'Em teste'),
+(4, 'Concluido');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_status`
+--
+ALTER TABLE `tb_status`
+  ADD PRIMARY KEY (`id_status`);
+  
+
+CREATE TABLE `tb_atividade` (
+  `id_atividade` int(10) UNSIGNED NOT NULL,
+  `id_status` int(10) UNSIGNED NOT NULL,
+  `nm_atividade` varchar(255) NOT NULL,
+  `ds_atividade` text NOT NULL,
+  `dt_inicio` date NOT NULL,
+  `dt_fim` date DEFAULT NULL,
+  `situacao` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `tb_atividade`
+  ADD PRIMARY KEY (`id_atividade`),
+  ADD KEY `tb_atividade_FKIndex1` (`id_status`);
+  
+ALTER TABLE `tb_atividade`
+  ADD CONSTRAINT `fk_id_status` FOREIGN KEY (`id_status`) REFERENCES `tb_status` (`id_status`);
